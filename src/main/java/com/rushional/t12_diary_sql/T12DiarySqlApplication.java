b77da3b7896b3713b058f09b4b46ec6dcc44fc72
Rushional <Rushional2@gmail.com>
2022-12-14 13:45:47 +0400

    first commit

---
+++ src/main/java/com/rushional/t12_diary_sql/T12DiarySqlApplication.java
@@ -0,0 +1,13 @@
+package com.rushional.t12_diary_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T12DiarySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T12DiarySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rushional/t12_diary_sql/T12DiarySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushional.t12_diary_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T12DiarySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
